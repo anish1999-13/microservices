fb0c1468c56af2d92339cad270d807fe322f009d
anish1999-13 <58840551+anish1999-13@users.noreply.github.com>
2026-09-01 15:14:43 -0400

    CRUD operations

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,20 @@
+package com.eazybytes.accounts.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    @NotEmpty(message = "AccountNumber cannot be null of empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 disgits")
+    private Long accountNumber;
+
+    @NotEmpty(message = "AccountType cannot be null or empty")
+    private String accountType;
+
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
+    private String branchAddress;
+
+}
